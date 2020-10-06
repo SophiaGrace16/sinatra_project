714b8f0e6a51e05f42ef0c6518c2e5871e3d2a36
SophiaGrace16 <sophia.g.white@outlook.com>
2020-10-06 13:58:18 -0400

    dropping user table

--- db/migrate/20201006151705_change_character_column.rb
+++
@@ -1,5 +0,0 @@
-class ChangeCharacterColumn < ActiveRecord::Migration[5.2]
-  def change
-    change_column :characters, :class, :character_class
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_06_152223) do
+ActiveRecord::Schema.define(version: 2020_10_06_174551) do
 
   create_table "characters", force: :cascade do |t|
     t.string "character_name"
@@ -54,11 +54,6 @@ ActiveRecord::Schema.define(version: 2020_10_06_152223) do
     t.text "summary"
     t.string "story_link"
     t.integer "dm_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
   end
 
 end
