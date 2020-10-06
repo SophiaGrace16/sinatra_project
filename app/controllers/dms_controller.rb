1a3b3b96c1015ab1f0fb361709831d3221171333
SophiaGrace16 <sophia.g.white@outlook.com>
2020-10-06 14:10:54 -0400

    added functioning dm log in and signup, added to form displays

--- app/controllers/dms_controller.rb
+++ app/controllers/dms_controller.rb
@@ -1,4 +1,12 @@
 class DMsController < ApplicationController
+
+    get "/dms/login" do
+        erb :"dms/login"
+    end
+
+    get "/dms/signup" do
+        erb :"dms/signup"
+    end
 end
 
 #signup, login, logout
