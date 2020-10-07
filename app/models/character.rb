b1f5396f5e99cc914d15fe2c807a32324635f618
SophiaGrace16 <sophia.g.white@outlook.com>
2020-10-07 13:52:17 -0400

    adding character validations

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,5 @@
 class Character < ActiveRecord::Base
     belongs_to :player
+
+    validates :character_name, :character_class, :level, :race, :background, :strength, :dexterity, :consititution, :intelligence, :wisdom, :charisma, :character_story presence: true
 end
