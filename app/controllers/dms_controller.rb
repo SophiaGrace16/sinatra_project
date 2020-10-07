d212ece6e990d06a9ee651eb4a0c40deeb6ad601
SophiaGrace16 <sophia.g.white@outlook.com>
2020-10-07 14:22:52 -0400

    adding functionality to dm signup/login

--- app/controllers/dms_controller.rb
+++ app/controllers/dms_controller.rb
@@ -4,16 +4,35 @@ class DMsController < ApplicationController
         erb :"dms/login"
     end
 
+    post '/dms/login' do
+        @dm = DM.find_by(username: params[:username])
+        if @dm && @dm.authenticate(params[:password])
+            session[:dm_id] = @dm.id
+            redirect_if_dm_logged_in
+        else
+            redirect_if_dm_not_logged_in
+        end
+    end
+
     get "/dms/signup" do
-        @dm=DM.new
         erb :"dms/signup"
     end
 
     post "/dms" do
-        @dm=DM.create(params)
-        redirect to "/dms/#{@dm.id}"
+        @dm = DM.new(params)
+        if @dm && @dm.save
+            session[:dm_id] = @dm.id
+            redirect "/stories"
+        else
+        erb :"dms/signup"
+        end
+ 
     end
 
+    get "/logout" do
+        session.clear
+        redirect to "/login"
+    end
 
 
 end
