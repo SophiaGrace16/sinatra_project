147d265edab3875abe75248389d727e705fab7c2
SophiaGrace16 <sophia.g.white@outlook.com>
2020-10-07 14:00:28 -0400

    added validations to dms and players

--- app/models/dm.rb
+++ app/models/dm.rb
@@ -1,6 +1,9 @@
 class DM < ActiveRecord::Base
     has_many :stories 
     has_secure_password
+
+    validates :name, :username, :password, presence: true
+    validates :name, :username, uniqueness: true
 end
 
 
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,7 @@
 class Player < ActiveRecord::Base
     has_many :characters
     has_secure_password 
+
+    validates :name, :username, :password, presence: true
+    validates :name, :username, uniqueness: true
 end
