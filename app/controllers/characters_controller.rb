671c607c96458a362ec925cb276c567ec22a7c18
SophiaGrace16 <sophia.g.white@outlook.com>
2020-10-07 13:52:58 -0400

    editing methods to work with validations

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,18 +1,24 @@
 class CharactersController < ApplicationController
 
     get '/characters' do 
-        if logged_in?
+        redirect_if_player_not_logged_in
             @characters = current_player.characters # shows the index of characters
         erb :'characters/index'
-        end
     end
 
     get '/characters/new' do
+        redirect_if_player_not_logged_in
+        @player = current_player
         erb :'characters/new'
     end
 
+    get '/characters/:id' do 
+        redirect_if_player_not_logged_in
+        @characters = Character.find_by_id(params[:id])
+        erb :'characters/show'
+    end
+
     post '/characters/new' do
-        binding.pry
         character = Character.new(params)
         if character.save
             redirect to "/characters/#{@character.id}"
@@ -21,26 +27,32 @@ class CharactersController < ApplicationController
         end
     end
 
-    get '/characters/:id' do 
-        @characters = Character.find_by_id(params[:id])
-        erb :'characters/show'
-    end
-
     get '/characters/:id/edit' do
+        redirect_if_player_not_logged_in
         @characters = Character.find_by_id(params[:id])
-        erb :'characters/edit'
+        if @character.user.id == current_player.id
+            erb :'characters/edit'
+        else
+            redirect to "/characters"
+        end
     end
 
     patch '/characters/:id' do
         @characters = Character.find_by_id(params[:id])
-        @character.update(params[:character])
-        redirect to "/characters/#{@character.id}"
+        params.delete("_method")
+        if @character.update(params)
+            redirect to "/characters/#{@character.id}"
+        else
+            redirect to "/characters/new"
+        end
     end
     #35:17 Live Lecture User authen
 
     delete "/character/:id" do
         @character = Character.find_by_id(params[:id])
-        @character.destroy(params[:id])
+        if @characer.user.id == current_user.id
+            @character.destroy
+        end
         redirect to "/characters"
     end  
 
